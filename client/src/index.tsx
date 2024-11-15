6bc4f98b5cafb731ef2732dc85eeb6873a59ce3d
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-11-15 08:59:15 +0000

    [structure] initial commit MERN stack

---
+++ client/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      main
+    </main>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
---
+++ server/src/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const ROOT = path.resolve(__dirname, "../../");
+const app = express();
+
+// express middlewares for url and json format
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const apiPath = path.join(ROOT, '/server/src/api');
+for(let endpoint of fs.readdirSync(apiPath)) {
+  let imported = await import(path.join(apiPath, endpoint));
+  imported.default(app, ROOT, database);
+}
+
+// serve static files from the React app
+app.use(express.static(path.join(ROOT, "/client/dist")));
+
+// serve the React app for any route !== /api
+app.get("*", (req, res) => {
+  res.sendFile(path.join(ROOT, "/client/dist/index.html"));
+});
+
+const PORT = process.env.port || 8080;
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`)
