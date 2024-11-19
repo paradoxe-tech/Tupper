69230593ca4a6ec6250b32cd61cbe1ef0edbd54b
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-11-19 23:07:41 +0000

    [server] added relation backlink complete

--- server/src/api/contacts.ts
+++ server/src/api/contacts.ts
@@ -1,12 +1,32 @@
 import fs from 'fs';
 import path from 'path';
+import { identifier } from '../../../shared/identifier';
+import { Contact } from '../../../shared/types';
 
 export default function run(app, ROOT:string) {
   app.get("/api/contacts", async (req, res) => {
     try {
       let dataPath = path.join(ROOT, '/data/contacts.json');
-      let data = fs.readFileSync(dataPath);
-      data = JSON.parse(data);
+      let data: Contact[] = JSON.parse(fs.readFileSync(dataPath, {
+        encoding: "utf-8"
+      }));
+
+      for(let contact of data) {
+        let contactIdent = identifier(contact)
+        for(let [type, targetIdent] of contact.relations) {
+          let target = data.find(c => identifier(c) == targetIdent)
+          if(target) {
+            let relType = `~${type}`.replaceAll('~~', '')
+            let relExists = target.relations
+              .find(r => r[0] == relType && r[1] == contactIdent)
+            
+            if(!relExists) {
+              target.relations.push([relType, contactIdent])
+            }
+          }
+        }
+      }
+      
       res.json(data)
     } catch (err) {
       res.status(500).send(err)
