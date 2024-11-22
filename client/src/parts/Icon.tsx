283e0be89bacc6a926cf3a470fa59d3db43c6385
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-11-22 00:24:39 +0000

    [client] added a "fill" property to Icon that uses ion's -outline

--- client/src/parts/Icon.tsx
+++ client/src/parts/Icon.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Icon: React.FC<{ name: string }> = ({ name }) => {
+export const Icon: React.FC<{ name: string, fill: boolean }> = ({ name, fill=false }) => {
   return (
     <div className="relative top-[2px] mr-3 inline-block">
-      <ion-icon name={name}></ion-icon>
+      <ion-icon name={`${name}${fill ? '' : '-outline'}`}></ion-icon>
     </div>
   );
 };
